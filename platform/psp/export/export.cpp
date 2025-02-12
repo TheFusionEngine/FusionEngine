5090679d659bb106d86a6dd99f172e86bed03d69
c08oprkiua <85906487+c08oprkiua@users.noreply.github.com>
2025-02-11 19:48:23 -0700

    Update PSP platform

--- platform/psp/export/export.cpp
+++ platform/psp/export/export.cpp
@@ -4,17 +4,15 @@
 #include "tools/editor/editor_settings.h"
 #include "scene/resources/texture.h"
 
-class EditorExportPlatformPSP : public EditorExportPlatform {
-    OBJ_TYPE(EditorExportPlatformPSP, EditorExportPlatform);
+class EditorExportPlatformPSP : public EditorExportPlatformConsole {
+    OBJ_TYPE(EditorExportPlatformPSP, EditorExportPlatformConsole);
 private:
 
 public:
 
 	void _get_property_list( List<PropertyInfo> *p_list) const;
 
-	virtual String get_name() const;
 	virtual ImageCompression get_image_compression() const;
-	virtual Ref<Texture> get_logo() const;
 
 	virtual bool poll_devices() { return false; }
 	virtual int get_device_count() const { return 0; }
@@ -25,7 +23,6 @@ public:
 	virtual bool can_export(String *r_error=NULL) const;
 
 	virtual bool requires_password(bool p_debug) const { return false; }
-	virtual String get_binary_extension() const;
 	virtual Error export_project(const String& p_path,bool p_debug,bool p_dumb=false);
 };
 
@@ -36,27 +33,12 @@ void EditorExportPlatformPSP::_get_property_list( List<PropertyInfo> *p_list) co
 	p_list->push_back(PropertyInfo(Variant::STRING, "launcher/sound", PROPERTY_HINT_FILE, "mp3"));
 }
 
-String EditorExportPlatformPSP::get_name() const {
-    return "Playstation Portable";
-}
-
 EditorExportPlatform::ImageCompression EditorExportPlatformPSP::get_image_compression() const{
     return IMAGE_COMPRESSION_NONE;
 }
 
-Ref<Texture> EditorExportPlatformPSP::get_logo() const {
-	Image img(_psp_logo);
-	Ref<ImageTexture> logo = memnew(ImageTexture);
-	logo->create_from_image(img);
-    return logo;
-}
-
 bool EditorExportPlatformPSP::can_export(String *r_error) const {
     return false;
-};
-
-String EditorExportPlatformPSP::get_binary_extension() const {
-    return "PBP";
 };
 
 Error EditorExportPlatformPSP::export_project(const String& p_path,bool p_debug,bool p_dumb){
@@ -64,7 +46,14 @@ Error EditorExportPlatformPSP::export_project(const String& p_path,bool p_debug,
 };
 
 void register_psp_exporter(){
-	EDITOR_DEF("psp/embed_pck", "");
-    Ref<EditorExportPlatformPSP> exporter = Ref<EditorExportPlatformPSP>(memnew(EditorExportPlatformPSP));
+    Ref<EditorExportPlatformPSP> exporter = memnew(EditorExportPlatformPSP);
+
+	exporter->set_name("Playstation Portable");
+	exporter->set_binary_extension("PBP");
+	Image img(_psp_logo);
+	Ref<ImageTexture> logo = memnew(ImageTexture);
+	logo->create_from_image(img);
+	exporter->set_logo(logo);
+
 	EditorImportExport::get_singleton()->add_export_platform(exporter);
 }
